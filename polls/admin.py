adac04d8f2ae8ba4d80a9dcc0ac76e333016a80d
Gio Cadiz <giovannicadiz@gmail.com>
2018-01-22 14:06:39 -0400

    add class PollsAdmin, QuestionAdmin, ChoiceAdmin in admin.py

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from polls.models import Poll, Question, Choice
+
 
 # Register your models here.
+class PollsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'pub_date']
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['poll', 'question_text']
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['question', 'choice_text', 'votes']
+
+
+
+admin.site.register(Poll, PollsAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
